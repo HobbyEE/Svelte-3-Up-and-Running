0e71d202b1e38ff3f464796dbf0317496607b559
hobbyee <nkalousis@gmail.com>
2022-01-06 20:28:51 +0200

    computer save

---
+++ svelte-poc/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let name = ''
+</script>
+
+<h1>Hello, {name}</h1>
+<p>My first svelte app</p>
+<p>let's try it out....</p>
+
+<style>
+  p {
+    color: #1d4585;
+    margin-left: 24px;
+  }
+</style>
